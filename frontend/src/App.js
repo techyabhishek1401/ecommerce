086484a8f37b328e485241af966c36f50ad20778
techyAbhishek <techyabhishek1401@gmail.com>
2021-05-10 11:12:26 +0530

    day1 &2

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+// rafce tab
+// removed-App.test.js,app.css,logo.svg
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path="/" exact>
+            <HomeScreen />
+          </Route>
+          <Route path="/product/:id" component={ProductScreen} />
+
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product'
+import axios from 'axios';
+
+const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/products')
+            .then(res => setProducts(res.data))
+            .catch(err => console.log("error:", err))
+    }, [])
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map(product => (
+                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+
+            </Row>
+        </>)
+}
+
+export default HomeScreen
